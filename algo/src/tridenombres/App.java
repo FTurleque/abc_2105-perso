fee2b8e46e58539d727a3f96ab067dc9a61e9f32
fturleque <84319762+FTurleque@users.noreply.github.com>
2021-06-07 16:51:17 +0200

    Début tri de nombres

---
+++ algo/src/tridenombres/App.java
@@ -0,0 +1,34 @@
+package tridenombres;
+
+import java.util.Scanner;
+
+public class App {
+    
+    public static void main(String[] args) {
+        int a,
+            b,
+            c;
+        Scanner scanner;
+        
+        System.out.println("Tri de nombres !");
+        scanner = new Scanner(System.in);
+
+        System.out.println("Choisir un nombre pour a :");
+        a = scanner.nextInt();
+
+        System.out.println("Choisir un nombre pour b :");
+        b = scanner.nextInt();
+
+        System.out.println("Choisir un nombre pour c :");
+        c = scanner.nextInt();
+
+        if (a <= b) {
+            
+        } else {
+            
+        }
+
+        scanner.close();
+
+    }
+}
