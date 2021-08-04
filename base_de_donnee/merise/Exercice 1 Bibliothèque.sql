7e9d231e98ff8f15322dbdac5f19f099c23b7f8d
FTurleque <57303912+FTurleque@users.noreply.github.com>
2021-08-04 13:12:59 +0200

    Création Base de donnée Bibliothèque

---
+++ base_de_donnee/merise/Exercice 1 Bibliothèque.sql
@@ -0,0 +1,26 @@
+DROP DATABASE IF EXISTS db_library;
+
+CREATE DATABASE IF NOT EXISTS db_library;
+
+USE db_library;
+
+CREATE TABLE IF NOT EXISTS customers
+(
+	customer_id INT(11) PRIMARY KEY,
+    customer_name VARCHAR(64) NOT NULL,
+    customer_address VARCHAR(255) NOT NULL,
+    customer_deposit INT(11) NOT NULL,
+    customer_loans_date DATE NOT NULL,
+    customer_loans_nb INT(4) NULL,
+    customer_loans_late BOOLEAN NULL
+);
+
+CREATE TABLE IF NOT EXISTS books
+(
+	book_id INT(11) PRIMARY KEY,
+    book_title VARCHAR(64) NOT NULL,
+    book_publisher VARCHAR(32) NULL UNIQUE,
+    book_author VARCHAR(32) NULL,
+    book_purchasedate DATE NOT NULL,
+    book_condition VARCHAR(16)
+);
