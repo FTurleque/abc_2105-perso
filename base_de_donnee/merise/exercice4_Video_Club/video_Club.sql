fb434b0923d1b349c7a8d77c9c5829d82c259ea7
FTurleque <57303912+fab200981@users.noreply.github.com>
2021-08-23 22:11:33 +0200

    Création du vidéo club sql

--- base_de_donnee/merise/exercice4_Video_Club/video_Club.sql
+++ base_de_donnee/merise/exercice4_Video_Club/video_Club.sql
@@ -11,5 +11,29 @@ CREATE TABLE IF NOT EXISTS customers
     customer_lastname VARCHAR(64) NOT NULL,
     customer_address VARCHAR(255) NOT NULL,
     customer_borrow_number INT(4) NULL,
-    customer_deposit INT(11) NOT NULL,
-)
+    customer_deposit INT(11) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS borrow
+(
+    borrow_id INT(11) PRIMARY KEY,
+    borrow_late BOOLEAN NULL,
+    borrow_date DATE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS tapes
+(
+    tape_id INT(11) PRIMARY KEY,
+    movie_title VARCHAR(64) NOT NULL,
+    movie_actorfistname VARCHAR(64) NOT NULL,
+    movie_actorlastname VARCHAR(64) NOT NULL,
+    movie_time TIME NOT NULL,
+    movie_directorfistname VARCHAR(64) NOT NULL,
+    movie_directorlastname VARCHAR(64) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS typeofmovies
+(
+    typeofmovie_name VARCHAR(30) PRIMARY KEY,
+    typeofmovie_public VARCHAR(30) NULL
+);
