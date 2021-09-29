f0daa96dd384b70193f80139a4d1bd67b3dd77f3
fturleque <84319762+FTurleque@users.noreply.github.com>
2021-09-29 16:05:56 +0200

    Fin Exercice 2

--- web/javascript/exercices/ex2_4.js
+++ web/javascript/exercices/ex2_4.js
@@ -10,8 +10,8 @@ let find = false;
 let nbToFind = parseInt(Math.floor(Math.random() * ((max + 1) - min) + min));
 
 alert("Trouvez le nombre mystère en 10 essaient !")
-// askNumber = prompt("Choisir un nombre entre 1900 et 2021 :");
-// userNumber = parseInt(askNumber);
+askNumber = prompt("Choisir un nombre entre 1900 et 2021 :");
+userNumber = parseInt(askNumber);
 
 for (let i = 0; i < 10; i++) {
     askNumber = prompt("Choisir un nombre entre 1900 et 2021 :");
@@ -28,3 +28,20 @@ for (let i = 0; i < 10; i++) {
         i--;
     }
 }
+
+
+// do {
+//     askNumber = prompt("Choisir un nombre entre 1900 et 2021 :");
+//     userNumber = parseInt(askNumber);
+
+//     if (userNumber > 1900 && userNumber < 2021) {
+//         if (userNumber != nbToFind) {
+//             alert("Perdu ! Réessayez.");
+//         } else {
+//             alert("Bien jouez vous avez trouver le nombre mystère !");
+//         }
+//     } else {
+//         alert("Vous n'avez pas choisi un nombre compris entre 1900 et 2021, recommencer :");
+//         // i--;
+//     }
+// } while (userNumber != nbToFind);
--- web/javascript/exercices/ex2_5.js
+++ web/javascript/exercices/ex2_5.js
@@ -1,11 +1,23 @@
 // 6) Écrire un algorithme qui calcule la factorielle d'un nombre.
 
 let askNumber;
-let numberToFact = parseInt(askNumber);
-let factNb = [];
+let numberToFact;
+let factNb = new Array();
+let sumFactNb= 0;
 
-askNumber = prompt("Choisir un nombre pour en calculer le factorille");
-
-for (let index = 1; index <= numberToFact; index++) {
-
+askNumber = prompt("Choisir un nombre pour en calculer le factorielle");
+numberToFact = parseInt(askNumber);
+for (let index = 0; numberToFact > index; index++) {
+    // factNb[index] = (index + (index + 1));
+    let toto = (index + (index + 1));
+    factNb[index] = toto;
 }
+
+console.table(factNb);
+
+for (let index = 0; index < factNb.length; index++) {
+    // sumFactNb += factNb[index];
+    sumFactNb = sumFactNb + factNb[index];
+}
+
+document.write(`Le factorielle de ${numberToFact} est : ${sumFactNb}`);
